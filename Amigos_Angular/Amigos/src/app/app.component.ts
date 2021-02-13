9718cf40478eae17e1554e613224ab1ec473dd65
pacifiquebayisenge <32477950+pacifiquebayisenge@users.noreply.github.com>
2021-02-13 04:34:15 +0100

    dark mode

--- Amigos_Angular/Amigos/src/app/app.component.ts
+++ Amigos_Angular/Amigos/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Amigos';
+  title = 'A M I G O S';
+
+  isDarkTheme: boolean = true;
 }
--- Amigos_Angular/Amigos/src/app/app.module.ts
+++ Amigos_Angular/Amigos/src/app/app.module.ts
@@ -5,15 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
